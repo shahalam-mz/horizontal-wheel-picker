c1cdfda0c5619a20d3385cc89c068ae2dded7b02
Shah Alam <linktos.alam@gmail.com>
2024-08-17 22:39:59 +0500

    Project Setup

---
+++ app/src/main/java/com/ronin/horizontalwheelpicker/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ronin.horizontalwheelpicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.ronin.horizontalwheelpicker.ui.theme.HorizontalWheelPickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HorizontalWheelPickerTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Horizontal Wheel Picker")
+                }
+            }
+        }
+    }
+}
