8b43fe23a927db67b461aaebf11f53ef6860c19b
Shah Alam <linktos.alam@gmail.com>
2024-08-18 00:12:31 +0500

    Horizontal wheel picker integration

--- horizontal-wheel-picker/build.gradle.kts
+++ horizontal-wheel-picker/build.gradle.kts
@@ -1,23 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
-    namespace = "com.ronin.horizontalwheelpicker"
+    namespace = "com.ronin.horizontal_wheel_picker"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ronin.horizontalwheelpicker"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -61,12 +55,12 @@ dependencies {
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
-    // Horizontal Wheel Picker
-    implementation(project(":horizontal-wheel-picker"))
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.svg)
+
+    // SDP/SSP Compose
+    api(libs.sdp.compose)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,8 +16,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "Horizontal Wheel Picker"
 include(":app")
+include(":horizontal-wheel-picker")
